2edab79609e8fd7f2795ac7c638a6033323348ff
Kun Wang <kunwang034@gmail.com>
2026-06-12 10:37:34 +0800

    Add Read the Docs build configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,10 @@ exclude_patterns = []
 add_module_names = False
 source_suffix = [".rst", ".md"]
 autoapi_dirs = ['../../phylovelo']
+nbsphinx_execute = 'never'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
